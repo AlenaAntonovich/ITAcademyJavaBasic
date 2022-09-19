3f5629acdb48a25157955c984fab65e9c61f986b
AlenaAntonovich <alyona28091989@yandex.ru>
2022-09-19 21:25:33 +0300

    hm1

---
+++ src/Homework1.java
@@ -0,0 +1,20 @@
+public class Homework1 {
+    public static void main(String[] args) {
+        byte b = 0x55;
+        System.out.println("byte b = " + b);
+        short s = 0x55ff;
+        System.out.println("short s = " + s);
+        int i = 1000000;
+        System.out.println("int i = " + i);
+        long l = 0xffffffffL;
+        System.out.println("long l = " + l);
+        char c = 'a';
+        System.out.println("char c = " + c);
+        float f = .25f;
+        System.out.println("float f = " + f);
+        double d = .00001234;
+        System.out.println("double d = " + d);
+        boolean bool = true;
+        System.out.println("boolean bool = " + bool);
+    }
+}
